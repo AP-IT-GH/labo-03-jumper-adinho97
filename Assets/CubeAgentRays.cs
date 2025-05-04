115712ce3b3b5a6c3aa58e3aaa6c48092fad6f92
adinho97 <adrian.dys97@gmail.com>
2025-05-04 20:10:13 +0200

    Initial Unity project commit

---
+++ Assets/CubeAgentRays.cs
@@ -0,0 +1,61 @@
+    using UnityEngine;
+    using Unity.MLAgents;
+    using Unity.MLAgents.Sensors;
+    using Unity.MLAgents.Actuators;
+    using System.Collections.Generic;
+    public class CubeAgentRays : Agent
+    {
+        public Transform Target;
+
+        public override void CollectObservations(VectorSensor sensor)
+        {
+            sensor.AddObservation(this.transform.localPosition);    
+        }
+        public override void OnEpisodeBegin()
+        {
+            // reset de positie en orientatie als de agent gevallen is
+            if (this.transform.localPosition.y < 0)
+            {
+
+                this.transform.localPosition = new Vector3(0, 0.5f, 0); this.transform.localRotation = Quaternion.identity;
+            }
+
+            // verplaats de target naar een nieuwe willekeurige locatie 
+            Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+
+        }
+
+        public float speedMultiplier = 0.1f;
+        public float rotationMultiplier = 5;
+        public override void OnActionReceived(ActionBuffers actionBuffers)
+        {
+            // Acties:
+            Vector3 controlSignal = Vector3.zero;
+            controlSignal.z = actionBuffers.ContinuousActions[0];
+            transform.Translate(controlSignal * speedMultiplier);
+
+            // Voer rotatie uit
+            transform.Rotate(0, rotationMultiplier * actionBuffers.ContinuousActions[1], 0);
+
+            // Beloningen
+            float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+            if (distanceToTarget < 1.42f)
+            {
+                SetReward(1.0f);
+                EndEpisode();
+            }
+            else if (this.transform.localPosition.y < 0)
+            {
+                EndEpisode();
+            }
+        }
+
+
+        public override void Heuristic(in ActionBuffers actionsOut)
+        {
+            var continuousActionsOut = actionsOut.ContinuousActions;
+            continuousActionsOut[0] = Input.GetAxis("Vertical");
+            continuousActionsOut[1] = Input.GetAxis("Horizontal");
+        }
+    }
